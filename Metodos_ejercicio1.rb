f2840481cced8f84b1aaf5ae44f5d1c5b7550c8c
Matias Jara <mjara.figueroa@gmail.com>
2017-08-02 14:38:08 -0400

    Ejercicio 1 de ruby metodos

---
+++ Metodos_ejercicio1.rb
@@ -0,0 +1,8 @@
+# El   siguiente   método   debería   devolver   la   suma   de   2
+# números, sin embargo solo los muestra en pantalla.
+
+def suma(a, b)
+    return a + b
+end
+result = suma(2, 3)
+puts result + 5 # Debería mostrar 10

61d71d9817b970a14c77fcc0a277a6f02ef44ea0
Matias Jara <mjara.figueroa@gmail.com>
2017-08-02 15:06:12 -0400

    arreglando nombre

--- Metodos_ejercicio1.rb
+++
@@ -1,8 +0,0 @@
-# El   siguiente   método   debería   devolver   la   suma   de   2
-# números, sin embargo solo los muestra en pantalla.
-
-def suma(a, b)
-    return a + b
-end
-result = suma(2, 3)
-puts result + 5 # Debería mostrar 10

cc7025b652820d96bf59b7b21e7d4f3aa7e5c8cc
Matias Jara <mjara.figueroa@gmail.com>
2017-08-02 14:41:40 -0400

    Ejercicio 2 de ruby metodos

---
+++ Metodos_ejercicios2.rb
@@ -0,0 +1,6 @@
+# Se   pide   modificar   el   siguiente   método   para   que   el
+# segundo argumento pueda tener un valor por defecto de 1
+
+def increment(value, amount = 1)
+    return value + amount
+end
